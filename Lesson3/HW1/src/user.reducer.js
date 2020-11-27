d5e351efedf9408df0cdd1596167187aa55d85a4
Leonid <leonidpistryi@gmail.com>
2020-11-27 07:11:14 +0200

    e

--- Lesson3/HW1/src/user.reducer.js
+++ Lesson3/HW1/src/user.reducer.js
@@ -6,7 +6,7 @@ const initialState = {
 
 export const userReducer = (state = initialState, action) => {
     switch(action.type){
-        case ADD_USER:{
+        case SET_USER:{
             return {
                 ...state,
                 ...action.payload.userData,
