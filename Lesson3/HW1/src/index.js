22639fb438a038b67c526966ff0195645c9bc3ef
Leonid <leonidpistryi@gmail.com>
2020-11-27 07:15:24 +0200

    3

--- Lesson3/HW1/src/index.js
+++ Lesson3/HW1/src/index.js
@@ -6,10 +6,10 @@ import { setUser, removeUser } from '../src/user.actions';
 
 store.subscribe(() => console.log(store.getState()))
 
-store.dispatch(addProduct({ id: 1, name: 'cheeps' }));
+store.dispatch(addProduct({ id: 76, name: 'milk' }));
 store.dispatch(addProduct({ id: 2, name: 'beer' }));
 store.dispatch(addProduct({ id: 3, name: 'cigarets' }));
-store.dispatch(removeProduct(3));
-store.dispatch(setUser({ hobby: 'footbal'}));
+store.dispatch(removeProduct(76));
+store.dispatch(setUser({ name: 'Bob'}));
 store.dispatch(removeUser(3));
 store.dispatch(setLanguage('jp'));
