a68b9feb1e71069b6683e5f01ecb02bd816c0d63
Leonid <leonidpistryi@gmail.com>
2020-12-03 04:16:21 +0200

    1

--- Lesson6/Task1/src/store.js
+++ Lesson6/Task1/src/store.js
@@ -1,26 +1,18 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import counterReducer from '../src/counter.reducer';
 
-const logger = store => next => action =>{
-    // console.log(action.type);
-    // console.group(action.type);
-    // console.info(action)
-    // const result = next(action);
-    // console.groupEnd(result)
-    // return next(action);
-
+const logger = (store) => (next) => (action) => {
     console.group(action.type);
-    console.info('dispatching', action);
+    console.info("dispatching", action);
     let result = next(action);
-    console.log('next state', store.getState());
+    console.log("next state", store.getState());
     console.groupEnd();
-    
     return result;
-}
+};
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore (
+const store = createStore(
     counterReducer,
     composeEnhancers(applyMiddleware(logger))
 );
